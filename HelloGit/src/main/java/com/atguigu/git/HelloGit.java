d14a5343556d94627e5d20f60524631939c7b158
KonYan <724343694@qq.com>
2020-10-09 22:57:32 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author KonYan
+ * @create 2020-10-09 22:48
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+
+    }
+}
